c7ffc76876942c5f8b8c97f9d0d49e3f326f3914
cpe2021-2024 <guth.moelle@cpe.fr>
2022-11-21 17:34:23 +0100

    update views/layout

--- views/layout/header.inc.php
+++ views/layout/header.inc.php
@@ -50,7 +50,7 @@
                         <?php if (isset($_SESSION['auth']) and ($_SESSION['auth']==true)) { ?>
                             <a class="dropdown-item" href="#">
                                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Mon Profile
+                                Mon Profil
                             </a>
                         <?php } ?>  
                     </li>
